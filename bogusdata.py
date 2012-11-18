77f1994f6d6cc124db5e8dbfae7c8415ac82e036
adamtron <adam@cyberdigitalsomething.com>
2012-11-18 01:44:43 -0500

    Added variable-interval scheduling for output. Testing for timestamps.

--- bogusdata.py
+++ bogusdata.py
@@ -1,51 +1,55 @@
 import argparse
 import random
 import threading
+import datetime
 from types import *
 
 
+MS_SECOND = 1000
+
+
 class DataPoint():
-    name = None
-    data_type = None
-    gen_fx = None
-    gen_fx_kargs = None
+    _name = None
+    _data_type = None
+    _gen_fx = None
+    _gen_fx_kargs = None
 
     def __init__(self,
                  name,
                  data_type=FloatType,
                  gen_fx=random.random,
                  gen_fx_kargs=None):
-        self.name = name
-        self.data_type = data_type
-        self.gen_fx = gen_fx
-        self.gen_fx_kargs = gen_fx_kargs
+        self._name = name
+        self._data_type = data_type
+        self._gen_fx = gen_fx
+        self._gen_fx_kargs = gen_fx_kargs
 
     def data_gen(self):
-        if self.gen_fx_kargs is None:
-            return self.data_type(self.gen_fx())
+        if self._gen_fx_kargs is None:
+            return self._data_type(self._gen_fx())
         else:
-            return self.data_type(self.gen_fx(**self.gen_fx_kargs))
+            return self._data_type(self._gen_fx(**self._gen_fx_kargs))
 
     def __str__(self):
-        return ' '.join((str(self.name),
-                         str(self.data_type),
-                         str(self.gen_fx),
-                         str(self.gen_fx_kargs)))
+        return ' '.join((str(self._name),
+                         str(self._data_type),
+                         str(self._gen_fx),
+                         str(self._gen_fx_kargs)))
 
 
 class BogusDataGenerator():
-    points = list()
+    _points = list()
 
     def __init__(self):
         pass
 
     def append(self, datapoint):
-        self.points.append(datapoint)
+        self._points.append(datapoint)
 
     def data_gen(self):
         datas = list()
 
-        for point in self.points:
+        for point in self._points:
             datas.append(point.data_gen())
 
         return datas
@@ -56,48 +60,78 @@ class ScheduledBogusDataGenerator(BogusDataGenerator):
     _end_callback = None
 
     _timer = None
-    _interval_ms = 1000
-    
+    _interval_ms = MS_SECOND
+
     _output_size = 0
     _iterations = 0
 
-    def start_fixed_interval(self, output_callback, interval_ms, end_callback=None):
+    def __init__(self, output_size=0):
+        self._output_size = output_size
+
+    def start_interval_fixed(self,
+                             output_callback,
+                             interval_ms,
+                             end_callback=None):
+
         self._output_callback = output_callback
-        self._interval_ms = interval_ms
-        
+
+        class DummyIter():
+            def __init__(self, interval):
+                self._interval = interval
+
+            def next(self):
+                return self._interval
+
+        self._interval_iter = DummyIter(interval_ms)
+
         if end_callback is None:
-            self._end_callback = self._def_end_callback
+            self._end_callback = self._default_end_callback
         else:
             self._end_callback = end_callback
-            
+
+        self._schedule()
+
+    def start_interval_var(self,
+                           output_callback,
+                           interval_iter,
+                           end_callback=None):
+
+        self._output_callback = output_callback
+        self._interval_iter = interval_iter
+
+        if end_callback is None:
+            self._end_callback = self._default_end_callback
+        else:
+            self._end_callback = end_callback
+
         self._schedule()
 
     def cancel(self):
         self._timer.cancel()
         self._end_callback()
 
-    def __init__(self, output_size):
-        self._output_size = output_size
-
-    def _def_end_callback(self):
+    def _default_end_callback(self):
         pass
+
+    def _next_interval(self):
+        return self._interval_iter.next() / MS_SECOND
 
     def _do_output(self):
         self._iterations += 1
         datas = self.data_gen()
-        
+
         if self._iterations < self._output_size:
             self._schedule()
-            
+
         self._output_callback(datas)
-        
+
         #Do a seperate check after callbacks so our timing isn't messed up
-        #by slow callback clients 
+        #by slow callback clients
         if self._iterations >= self._output_size:
-            self._end_callback()        
+            self._end_callback()
 
     def _schedule(self):
-        self._timer = threading.Timer(self._interval_ms / 1000,
+        self._timer = threading.Timer(self._next_interval(),
                                       self._do_output)
         self._timer.start()
 
@@ -107,27 +141,17 @@ def local_callback(results):
 
 
 def local_end_callback():
-    print('DONE!!!')
+    print('Done.')
+    exit()
+
 
 def main():
     """
-    Basic testing for outputs   
+    Basic testing for outputs
     """
 
-#    arg_parser = argparse.ArgumentParser(description='Bogus Data Generator')
-#    arg_parser.add_argument('input_file')
-#    arg_parser.add_argument('output_file')
+    generator = ScheduledBogusDataGenerator(output_size=100)
 
-#    args = arg_parser.parse_args()
-#    print args.input_file, args.output_file
-
-    generator = ScheduledBogusDataGenerator(10)
-
-    #Ex: Basic gaussian distribution fx
-    generator.append(DataPoint('Input1',
-                               FloatType,
-                               random.gauss,
-                               {"mu": 0.1, "sigma": 0.2}))
 
     #Ex: Custom value generator
     class FakeRandom():
@@ -137,9 +161,22 @@ def main():
             x, self.i = self.i, self.i + 1
             return x
 
+        def next(self):
+            return 1000 * (random.weibullvariate(0.1, 0.345))
+
+        def timestampgen(self):
+            return str(datetime.datetime.now())
+
+    fakernd = FakeRandom()
+
+
+    generator.append(DataPoint('Input1',
+                               data_type=StringType,
+                               gen_fx=fakernd.timestampgen))
+
     generator.append(DataPoint('Input2',
                                data_type=FloatType,
-                               gen_fx=FakeRandom().fake_random))
+                               gen_fx=fakernd.fake_random))
 
     #Ex: IntType returned with stock randint and kargs
     generator.append(DataPoint('Input3',
@@ -152,7 +189,19 @@ def main():
                                gen_fx=random.gauss,
                                gen_fx_kargs={"mu": 0.99, "sigma": 0.01}))
 
-    generator.start_fixed_interval(local_callback, 1000, end_callback=local_end_callback)
+    #Ex: Basic gaussian distribution fx
+    generator.append(DataPoint('Input5',
+                               FloatType,
+                               random.gauss,
+                               {"mu": 0.1, "sigma": 0.2}))
+
+#    generator.start_interval_fixed(local_callback,
+#                                   1000,
+#                                   end_callback=local_end_callback)
+
+    generator.start_interval_var(local_callback,
+                                 fakernd,
+                                 end_callback=local_end_callback)
 
 if __name__ == "__main__":
     main()
