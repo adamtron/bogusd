5fcb032d95dde2e5a049a912b4c30a5fced9c61b
adamtron <adam@cyberdigitalsomething.com>
2012-12-04 22:57:25 -0500

    Generator.generate now returns tupple <point.name, point.value>

--- bogusd.py
+++ bogusd.py
@@ -2,22 +2,23 @@ import random
 from threading import Thread, Event
 from types import *
 
+
 class Point():
     """
-    Represents a single output of some data point of a specified type, 
-    with distribution/range specified by gen_fx (defaulting to built-in RNG) 
+    Represents a single output of some data point of a specified type,
+    with distribution/range specified by gen_fx (defaulting to built-in RNG)
     """
     def __init__(self, name, data_type=FloatType, gen_fx=random.random,
                  gen_fx_args=[], gen_fx_kargs={}):
         self.name = name
         self.data_type = data_type
         self.gen_fx = gen_fx
-        self.gen_fx_args = gen_fx_args        
+        self.gen_fx_args = gen_fx_args
         self.gen_fx_kargs = gen_fx_kargs
 
     def data_gen(self):
         """Return the next value for this datapoint"""
-        return self.data_type(self.gen_fx(*self.gen_fx_args, 
+        return self.data_type(self.gen_fx(*self.gen_fx_args,
                                           **self.gen_fx_kargs))
 
     def __str__(self):
@@ -30,7 +31,7 @@ class Point():
 class Generator():
     """
     Aggregator for an arbitrary set of Points, returns an ordered list of
-    outputs based on Point order
+    (<name>,<value>) tupples generated from the Generator's set of points
     """
     def __init__(self):
         self.points = list()
@@ -42,7 +43,7 @@ class Generator():
         datas = list()
 
         for point in self.points:
-            datas.append(point.data_gen())
+            datas.append((point.name, point.data_gen()))
 
         return datas
 
@@ -50,38 +51,38 @@ class Generator():
 class _ScheduledData(Thread):
     """
     Thread that generates data and calls callback(data) at intervals specified
-    by interval_iterator. 
+    by interval_iterator.
     Inspired by threading.Timer
     """
-    def __init__(self, callback, datagen, interval_iterator, 
+    def __init__(self, callback, datagen, interval_iterator,
                  end_callback=None):
         Thread.__init__(self)
-        
+
         self.data_gen = datagen
         self.callback = callback
         self.interval_iter = interval_iterator
-        
+
         self.signal = Event()
-        
+
         if end_callback is None:
-            self.end_callback = self._default_on_end 
-        else: 
-            self.end_callback = end_callback  
+            self.end_callback = self._default_on_end
+        else:
+            self.end_callback = end_callback
 
     def cancel(self):
         self.signal.set()
-        
+
     def run(self):
         for interval in self.interval_iter:
             self.signal.wait(interval)
-            
-            if self.signal.is_set():
+
+            if self.signal.isSet():
                 self.end_callback()
                 return
-            
+
             datas = self.data_gen.generate()
             self.callback(datas)
-            
+
         self.end_callback()
 
     def _default_on_end(self):
@@ -94,21 +95,21 @@ def IntervalScheduler(callback, datagen, interval_iter, end_callback=None):
     interval_iter.next() intervals.
     When scheduler halts, will call end_callback
     """
-    
+
     return _ScheduledData(callback, datagen, interval_iter, end_callback)
 
 
-def FixedIntervalScheduler(callback, datagen, interval, end_callback=None, 
+def FixedIntervalScheduler(callback, datagen, interval, end_callback=None,
                            output_size=0):
     """
     Convenience method to create scheduled data output every -interval- seconds
     """
-    
+
     def interval_fx():
         return interval
-    
-    return IntervalScheduler(callback, datagen, 
-                             IntervalIterator(interval_fx, output_size), 
+
+    return IntervalScheduler(callback, datagen,
+                             IntervalIterator(interval_fx, output_size),
                              end_callback)
 
 
@@ -119,27 +120,27 @@ def IntervalIterator(interval_fx, size=0, fx_args=[], fx_kargs={}):
     have occured.
     Care should be taken using for-in statements when size == 0 as this will
     (intentionally) return values idefinitely.
-    """  
-    
+    """
+
     class DummyIterator():
         def __init__(self, next_fx, size, fx_args, fx_kargs):
             self.next_fx = next_fx
             self.args = fx_args
             self.kargs = fx_kargs
             self.size = size
-            self.count = 0            
+            self.count = 0
 
         def next(self):
             if self.size == 0:
                 return self.next_fx(*self.args, **self.kargs)
-            
+
             if self.count < self.size:
                 self.count += 1
                 return self.next_fx(*self.args, **self.kargs)
-            
+
             raise StopIteration()
-        
+
         def __iter__(self):
             return self
-        
-    return DummyIterator(interval_fx, size, fx_args, fx_kargs)    
+
+    return DummyIterator(interval_fx, size, fx_args, fx_kargs)
